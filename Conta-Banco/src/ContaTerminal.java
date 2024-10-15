3deb70edfe0f0c41b35611effb363202f218e485
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-15 08:06:42 -0300

    Realizado e vamos testar

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -16,8 +16,28 @@ public class ContaTerminal {
         String nomeCompleto = scanner.next();
         
         System.out.println("Digite o número da sua conta: ");
-        Int numeroConta = 
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite a sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Quanto você deseja colocar de saldo na sua conta? ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Estamos validando seus dados...");
+        Thread.sleep(2000);
+
+
+        System.out.println("Bem vindo " + nomeCompleto + " sua conta foi criada com sucesso!");
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Conta: " + numeroConta);
+        System.out.println("Saldo atual: " + saldo);
+
+        System.out.println("Caso queira algo a mais favor retornar ao menu inicial!");
+        System.out.println("Obrigado e tenha uma boa semana!");
         
+
+
         //Exibir a mensagem conta criada
     }
 }
