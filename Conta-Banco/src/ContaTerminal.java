33b1ea6f341954a6e584ed5856545db18b23365c
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-15 07:57:18 -0300

    Começando oprojeto

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        // TODO: Conhecer e importar a classe Scanner
-
+        //Conhecer e importar a classe Scanner
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         //Exibir as mensagens para o nosso usuário
-
+        System.out.println("Bem vindo ao banco Digital Inovation One!");
+        System.out.println("Por favor digite seu nome completo");
         //Obter pela scanner os valores digitados no terminal
-
+        String nomeCompleto = scanner.next();
+        
+        System.out.println("Digite o número da sua conta: ");
+        Int numeroConta = 
+        
         //Exibir a mensagem conta criada
     }
 }
